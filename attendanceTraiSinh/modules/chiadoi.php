764e039d1f91d5da8901f7f32077ea558526d882
Your Name <you@example.com>
2026-01-23 22:22:50 +0700

    update

--- attendanceTraiSinh/modules/chiadoi.php
+++ attendanceTraiSinh/modules/chiadoi.php
@@ -4,6 +4,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once __DIR__ . '/../../config/session.php';
 require_once __DIR__ . '/../config/db.php'; // $pdo
+// 1. Kiểm tra quyền truy cập
 if (!in_array($_SESSION['role'], ['admin','club_leader'])) {
     die('Không có quyền');
 }
